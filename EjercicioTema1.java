0aa35bd30a46402500a231713e30fbea4b64598e
adribanez <120591858+adribanez@users.noreply.github.com>
2022-12-14 18:57:18 +0100

    Add files via upload

---
+++ EjercicioTema1.java
@@ -0,0 +1,59 @@
+package com.ejerciciosBootcamp;
+
+public class EjercicioTema1 {
+
+    public static void main(String[] args) {
+
+                                        // tipos de datos
+
+        // 1- numericos
+
+
+                 // 1.1- enteros
+
+                        byte variable1;
+
+                                System.out.println("byte");
+                        short variable2;
+
+                                System.out.println("short");
+                        int variable3;
+
+                                System.out.println("int");
+                        long variable4;
+
+                                System.out.println("long");
+
+
+                 // 1.2- decimales
+
+                        float variable5;
+
+                                System.out.println("float");
+                        double variable6;
+
+                                System.out.println("double");
+
+                 // 2- booleano
+
+                        boolean variable7;
+
+                                System.out.println("booleanTrue");
+                        boolean variable8;
+
+                                System.out.println("booleanFalse");
+
+                 // 3- texto
+
+                        char variable9;
+
+                                System.out.println("char");
+                        String variable10;
+
+                                System.out.println("string");
+
+
+
+    }
+
+}
