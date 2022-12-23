2c1e3985005152a5f6d3faa99f56fc22d29c13c3
adribanez <120591858+adribanez@users.noreply.github.com>
2022-12-23 18:40:18 +0100

    Add files via upload

---
+++ ConcatenacionDeTextos.java
@@ -0,0 +1,15 @@
+package com.ejerciciosBootcamp;
+
+public class ConcatenacionDeTextos {
+    public static void main(String[] args) {
+
+
+
+        String[] nombres = {"Adrian", "Juan", "Lucia",};
+        for (int i = 0; i < nombres.length; i++){
+            System.out.println(nombres[i]);
+        }
+
+
+    }
+}
